0be39f11c44d267a909a7d070d6de248ecce4a6f
Jawaid Ali <160029584+jawaidali735@users.noreply.github.com>
2024-03-28 02:23:36 +0500

    push my atm program files to github

---
+++ project02_atm/index.ts
@@ -0,0 +1,82 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 10000;
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt(
+    [
+        {
+            name:"pinCode",
+            type:"number",
+            message:"Enter your four degit pin code!!!"
+    
+        }
+    ]
+);
+
+if (pinAnswer.pinCode === myPin){
+    console.log("Correct pin Code");
+
+    let operationAns = await inquirer.prompt(
+        [
+            {
+                name:"operation",
+                type:"list",
+                message:"Enter your choice",
+                choices:["Fast Cash","Withdraw","Check Balance"]
+            }
+        ]
+    )
+
+    if (operationAns.operation === "Fast Cash"){
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name:"amount",
+                    type:"list",
+                    message:"Choice one of you amount",
+                    choices:["1000","5000","10000","15000","20000"]
+                }
+            ]
+        )
+        if(amountAns.amount > myBalance){
+            console.log("Insufficient Balance");
+            console.log(`Your current balance is ${myBalance}`);
+           
+        }
+        else{
+            myBalance-= amountAns.amount
+            console.log(`Your balance is ${myBalance}`)
+        }
+    }
+    else if(operationAns.operation === "Withdraw"){
+        let withdrawAns =await inquirer.prompt(
+            [
+                {
+                    name:"amount1",
+                    type:"number",
+                    message:"Enter your amount"
+                }
+            ]
+        )
+        if(withdrawAns.amount1 > myBalance){
+            console.log(`Insuficient Balance`);
+            console.log(`Your current balance is ${myBalance}`);
+        }
+        else {
+            myBalance -= withdrawAns.amount1;
+            console.log(`Your current balance is ${myBalance}`);
+        }
+        
+    }
+    else if(operationAns.operation === "Check Balance"){
+        console.log(`Your balance is ${myBalance}`);
+    }
+
+   
+}
+else{
+    console.log("Incorrect Pin code")
+}
