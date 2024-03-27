a1b665abb25e182a9c9aa3deacc02092344af4da
Jawaid Ali <160029584+jawaidali735@users.noreply.github.com>
2024-03-28 02:06:13 +0500

    push my files to github

---
+++ project01/index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome to the Game");
+
+let answer = await inquirer.prompt([{
+    name: "guess",
+    type: "number",
+    message: "Guess the number"
+
+}])
+
+if(answer.guess === randomNumber){
+    console.log("Congratualations! You gueesed right number.")
+}
+else{
+    console.log("Wrong, you guessed wrong number.")
+}
+
